fcbbe037c0993499ae23c10c3ad4c7d0257d080a
Malik Falana <104946127+MaFalana@users.noreply.github.com>
2022-09-18 10:31:12 -0400

    made php page for email

---
+++ sendEmail.php
@@ -0,0 +1,22 @@
+<?php
+    $email = ""; //inputted email address
+    $message = "";
+    $fName = ""; //first name
+    $lName = ""; // last name
+    $to = "malikfalana@icloud.com"; //my email address
+
+
+    if (isset($_POST['Send']))
+    {
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $fName = $_POST['firstName'];
+        $lName = $_POST['lastName'];
+        $headers = "From: ".$email.;
+
+        $txt = "You have recieved an e-mail from".$Fname .$Lname.".\n\n".$message;
+        
+        mail($to, $txt, $message, $headers);
+        header("Location:index.html");
+    }
+?>
